89f65301555570cebf8a1f893b2ce26587fa7960
nrikiji <nrikiji@gmail.com>
2018-11-19 22:23:47 +0900

    update demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -1,20 +1,35 @@
-//
-//  ViewController.swift
-//  Demo
-//
-//  Created by 直之力示 on 2018/11/19.
-//  Copyright © 2018年 直之力示. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NRDatePickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize
+        let picker = NRDatePicker(frame: CGRect(x: 20, y: 50, width: view.frame.width-20*2, height: 50))
+        picker.pickerDelegate = self
+        
+        // Set Start And End Date For String Format(yyyy-MM-dd)
+        let _ = picker.setData(string: "2018-01-01", "2018-12-31")
+        // let _ = picker.setData(date: startDate, endDate)
+        
+        // Custome UI
+        picker.borderStyle = UITextField.BorderStyle.line
+        picker.layer.borderColor = UIColor.lightGray.cgColor
+        picker.layer.borderWidth = 0.1
+        picker.textAlignment = NSTextAlignment.center
+        view.addSubview(picker)
     }
-
-
+    
+    func done(date: Date?) {
+        // result with date
+        // Optional(2018-01-11 15:00:00 +0000)
+    }
+    
+    func done(string: String?) {
+        // result with string
+        // Optional("2018-01-12")
+    }
+    
 }
 
